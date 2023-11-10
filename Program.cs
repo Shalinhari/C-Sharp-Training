851e0680fc293179c12d396e1c37334e896e208d
Shaolin hari <shalinhari65@gmail.com>
2023-11-10 16:09:09 +0530

    Generic

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class GenericProgram
+{
+    private string name;
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+    public String GetName()
+    {
+        return name;
+    }
+}
+class program { 
+    public static void Main(string[] args)
+    {
+        GenericProgram obj = new GenericProgram();
+        obj.SetName("Shalin");
+        Console.WriteLine(obj.GetName());   
+    }
+}
+
+
